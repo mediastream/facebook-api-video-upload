824a66815ed7a1c2be0182543a28140e85a877da
Reinaldo Díaz <reinaldo122@gmail.com>
2018-11-25 19:13:28 -0300

    Fixes promise issue

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function uploadChain(buffer, args, res, ids) {
 }
 
 function facebookApiVideoUpload(args) {
-	return streamToPromise(args.stream)		
+	return Promise.resolve(streamToPromise(args.stream))
 		.then(buffer => [buffer, apiInit(args, buffer.length)])
 		.spread((buffer, res) => {
 			const ids = [res.upload_session_id, res.video_id];
